f8f8bca77ac831c957d67956d699852d8bf4634e
theayushkumar <ayushkumarhjp@gmail.com>
2024-10-18 16:40:33 +0530

    done

--- js/main.js
+++ js/main.js
@@ -70,7 +70,7 @@
         responsiveClass: true,
         responsive: {
             0: {
-                items: 2
+                items: 1
             },
             768: {
                 items: 4
