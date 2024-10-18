d83b0b96637f28d1d4bf733d463dc9ab16d07c00
theayushkumar <ayushkumarhjp@gmail.com>
2024-10-18 13:33:49 +0530

    done

--- js/main.js
+++ js/main.js
@@ -53,6 +53,37 @@
     });
 
 
+    // our team carousel
+    $(".team-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav : false,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:3
+            }
+        }
+    });
+
+
     // packages carousel
     $(".packages-carousel").owlCarousel({
         autoplay: true,
